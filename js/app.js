b35dbbeb031af2848bbd12a50207cec52dbcb761
Bryan <bryan.tovantrang@gmail.com>
2017-10-25 11:17:34 +0200

    ac

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ function openSideBar() {
 function closeSideBar() {
     document.getElementById("sidebar").style.width = "0";
 }
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+       $('nav').addClass('fixed-header');
+    }
+    else {
+       $('nav').removeClass('fixed-header');
+    }
+});
