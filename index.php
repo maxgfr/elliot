ad9a680640329dd5f9d9f055ee901c92f81229bb
Maxime Golfier <maxgolfier@gmail.com>
2017-11-20 11:45:46 +0100

    i

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	//Voir les erreurs
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL | E_STRICT);
+
+	// Répertoire racine du MVC
+	$rootDirectory = dirname(__FILE__)."/mvc/";
+
+	// URI de la racine du MVC ( sans la query string )
+	$rootURI = explode("index.php" , $_SERVER['REQUEST_URI'])[0];
+
+	// chargement de la classe Autoload pour autochargement des classes
+	require_once($rootDirectory.'Config/Autoload.php');
+
+	try {
+		Autoload::load();
+	} catch(Exception $e){
+		require (Config::getVues()["default"]) ;
+	}
+
+	$ctrl = new ControleurFront();
+?>
---
+++ mvc/Controleur/ControleurVisitor.php
@@ -0,0 +1,42 @@
+<?php
+
+
+    /** @brief ControleurVisitor identifie l'action et appelle la méthode pour construire le modèle correspondant à l'action avec le rôle "visitor". Le controleur appelle aussi la Vue correspondante. Il ne gère pas les exceptions, qui remontent au Front Controller. */
+    class ControleurVisitor {
+
+        function __construct ($action){
+            ///On distingue des cas d’utilisation, suivant l’action
+            switch($action) {
+                case "auth" :
+                    $this->actionAuth();
+                    break;
+                case "validateAuth" :
+                    $this->actionValidateAuth();
+                    break;
+
+                default : // L'action indéfinie ( page par défaut , ici accueil )
+                    require(Config::getVues()["default"]);
+                    break;
+            }
+        }
+
+        /** @brief Implemente l’action "auth" : saisie du login/password */
+        private function actionAuth(){
+            $model=new Model(array());
+            require(Config::getVues()["authentification"]);
+        }
+
+        /** @brief Implemente l’action "validateAuth" : Validation du login/password et création de session. */
+        private function actionValidateAuth(){
+            ValidationRequest::validationLogin($dataError, $email, $password);
+            $model=Authentication::checkAndInitiateSession($email, $password, $dataError);
+            if($model->getError()===false){
+                require(Config::getVues()["defaultAdmin"]);
+            }else{
+                require(Config::getVues()["authentification"]);
+            }
+        }
+
+
+     }
+?>
