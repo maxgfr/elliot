1191ecce8a2aaae05738cfd4079f9ab3909f01bb
Marin Mouscadet <mmouscadet@outlook.fr>
2017-11-20 11:27:24 +0100

    Les modifications

--- views/UserPage.php
+++ views/UserPage.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: MarinMouscadet
- * Date: 20/11/2017
- * Time: 10:32
- */
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <?php include("header.php"); ?>
+    <?php include("iconBar.php"); ?>
+
+    <title>UserPage</title>
+</head>
+<body>
+    <?php include("sidebar.php"); ?>
+    <p>Utilisateur</p>
+    <label for="Name">Nom : </label><input type="text" name="Name" id="Name">
+    <label for="Prenom">Prénom : </label><input type="text" name="Prenom" id="Prenom">
+    <label for="Telephone">Numéro de téléphone : </label><input type="text" name="Telephone" id="Telephone">
+    <input type="image" name="modification" value="modification" src="../img/Enregistrer_les_modifications.png">
+</body>
+</html>
