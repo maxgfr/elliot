479219a7ac7f078461edd19897157fa428187b94
Maxime Golfier <maxgolfier@gmail.com>
2017-11-24 10:32:07 +0100

    i

---
+++ mvc/Vue/sidebar.php
@@ -0,0 +1,42 @@
+<div id="mySideBar" class="sidebar">
+
+			  <a href="Homeplan.php"> <img id="bigIcon" src="../img/ellIoT2.png"> </a>
+
+			  <p></p>
+
+			  <button>
+				<div class="menu-image">
+					<a href="Homeplan.php"> <img src="../img/homeplanIcon.png"/> </a>
+				</div>
+				<div class="menu-name">
+					Plan du domicile	
+				</div>
+			  </button>
+
+			  <button>
+				<div class="menu-image">
+					<a href="Dashboard.php"> <img src="../img/dashboardIcon.png"/> </a>
+				</div>
+				<div class="menu-name">
+					Tableau de bord
+				</div>
+			  </button>
+
+			  <button>
+				<div class="menu-image">
+					<a href="Sensor.php"> <img src="../img/sensorIcon.png"/> </a>
+				</div>
+				<div class="menu-name">
+					Capteurs/actionneurs
+				</div>
+			  </button>
+
+			  <button>
+				<div class="menu-image">
+					<a href="Support.php"> <img src="../img/supportIcon.png"/> </a>
+				</div>
+				<div class="menu-name">
+					Support
+				</div>
+			  </button>
+	</div>
