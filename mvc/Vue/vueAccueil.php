0546070d99e750410d1f07d05b58bfb5f2436272
Maxime Golfier <maxgolfier@gmail.com>
2017-11-28 09:53:27 +0100

    iii

--- mvc/Vue/vueAccueil.php
+++ mvc/Vue/vueAccueil.php
@@ -1,14 +1,3 @@
-<?php
-  // Répertoire racine du MVC
-  $rootDirectory = dirname(__FILE__)."/../../mvc/";
-  // chargement de la classe Autoload pour autochargement des classes
-  require_once($rootDirectory.'Config\Autoload.php');
-  try {
-      Autoload::load();
-  } catch(Exception $e) {
-      require (Config::getVues()["default"]) ;
-  }
-?>
 <!DOCTYPE html>
 <html>
   <head>
