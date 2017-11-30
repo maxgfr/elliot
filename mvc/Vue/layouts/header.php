aa2cfab31c8ffa66766eb9513d0e986b4a4e3c8c
florianvavelin <vavelinflorian@gmail.com>
2017-11-30 12:26:15 +0100

    Add notification box

--- mvc/Vue/layouts/header.php
+++ mvc/Vue/layouts/header.php
@@ -3,29 +3,61 @@
       <header>
         <span id="hamburgerIcon" onclick="setSideBarStatus()">&#9776;</span>
         <div class="header_right">
-          <ul class="notificationPopup">
-              <li>
-                  <span class="howManyNotif">2</span>
-                  <a href="vueNotifications.php"> <img src="../../img/notificationIcon.png" alt="Notification icon" title="Notifications"/> </a>
-                  <ul>
-                      <li class="title">Notifications</li>
-                      <li>
-                          <a href="#">Lorem ipsum dolor sit amet</a>
-                      </li>
-                      <li>
-                          <a href="#">Lorem ipsum dolor sit amet</a>
-                      </li>s
-                      <li class="seeAll">
-                          <a href="vueNotifications.php">See all notifications</a>
-                      </li>
-                  </ul>
-              </li>
-          </ul>
+            <div class="notificationPopup">
+                <span id="how_many_notif">2</span>
+                <img src="notificationIcon.png" alt="Notification Icon" onclick="setNotificationPopupStatus()">
+            </div>
 
-          <button class="buttonHeader" type="button" title="Profile">
-            <a href="vueProfil.php"> <img src="../../img/userIcon.png" alt="User icon" title="Page de profil"/>DomISEP </a>
-          </button>
-          <a href="vueConnexion.php"> <img src="../../img/logoutIcon.png" alt="Logout icon" title="Logout"/> </a>
+            <button class="buttonHeader" type="button" title="Profile">
+                <a href="vueProfil.php"> <img src="../../img/userIcon.png" alt="User icon" title="Page de profil"/>DomISEP </a>
+            </button>
+            <a href="vueConnexion.php"> <img src="../../img/logoutIcon.png" alt="Logout icon" title="Logout"/> </a>
+
+
+            <div id="container_notification">
+
+                <div id="notification_title">
+                    <span>Notifications</span>
+                </div>
+
+                <div id="notification_messages">
+                    <div id="first_message">
+                        <button>
+                            Il y a de la merde dans ton chauffage.
+                            En plus t'es moche.<br>
+                            <span class="notificationDate">29 Nov 2017</span>
+                        </button>
+                    </div>
+                    <div id="second_message">
+                        <button>
+                            Tes capteurs sont pourris.<br>
+                            <span class="notificationDate">25 Nov 2017</span>
+                        </button>
+                    </div>
+                    <div id="third_message">
+                        <button>
+                            Change de maison gros.<br>
+                            <span class="notificationDate">19 Nov 2017</span>
+                        </button>
+                    </div>
+                    <div id="fourth_message">
+                        <button>
+                            elliot c'est le best.<br>
+                            <span class="notificationDate">08 Oct 2017</span>
+                        </button>
+                    </div>
+                    <div id="fifth_message">
+                        <button>
+                            Vavelin le king.<br>
+                            <span class="notificationDate">17 Avr 1998</span>
+                        </button>
+                    </div>
+                </div>
+
+                <div id="notification_footer">
+                    <button onclick="window.location.href='../../Documents/login.html'">Voir toutes les notifications</button>
+                </div>
+            </div>
         </div>
       </header>
 
