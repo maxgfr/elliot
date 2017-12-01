6cbe4e185e118fb1c7bb199144328296d6ab570e
Maxime Golfier <maxgolfier@gmail.com>
2017-12-01 11:27:40 +0100

    iiii

--- mvc/Modeles/ModelUser.php
+++ mvc/Modeles/ModelUser.php
@@ -17,55 +17,43 @@
 			return $this->email;
 		}
 
-    /** @brief Insère un user en créant un nouvel ID dans la BD. */
-    public static function getModelUserCreate($inputArray) {
-        $model = new self(array());
-         //Vérification des erreurs si une vérification a été exigée
-         if (empty($model->dataError)) {
-             // Execution de la requête d'insertion' :
-             $queryResults = DataBaseManager::getInstance()->prepareAndExecuteQueryAssoc("REPLACE INTO users(last_name,first_name,phone_number,birthday,mail,mail) VALUES (:last_name,:first_name,:phone_number,:birthday,:mail,:password)", $inputArray);
-             if ($queryResults === false) {
-                 $model->dataError["persistance"] = "Probleme d'execution de la requête";
-             }
-         } else {
-             $model->dataError = array_merge($model->dataError, $dataErrorAttributes); // fusion
-         }
-        $model->nom = "L'utilisateur a été ajouté à la BDD !";
-        return $model;
-    }
+	    /** @brief Insère un user en créant un nouvel ID dans la BD. */
+	    public static function getModelUserCreate($inputArray) {
+	        $model = new self(array());
+	         //Vérification des erreurs si une vérification a été exigée
+	         if (empty($model->dataError)) {
+	             // Execution de la requête d'insertion' :
+	             $queryResults = DataBaseManager::getInstance()->prepareAndExecuteQueryAssoc("REPLACE INTO users(last_name,first_name,phone_number,birthday,mail,mail) VALUES (:last_name,:first_name,:phone_number,:birthday,:mail,:password)", $inputArray);
+	             if ($queryResults === false) {
+	                 $model->dataError["persistance"] = "Probleme d'execution de la requête";
+	             }
+	         } else {
+	             $model->dataError = array_merge($model->dataError, $dataErrorAttributes); // fusion
+	         }
+	        $model->nom = "L'utilisateur a été ajouté à la BDD !";
+	        return $model;
+	    }
 
-	/** @brief Connexion d'un user */
-    public static function getModelConnexion($mail, $hashedPassword) {
-		$model = new self(array());
-		// Exécution de la requête via la classe de connexion (singleton). Le exceptions éventuelles, personnalisées, sont gérés par le Contrôleur
-        $args = array($mail, $hashedPassword);
-        $queryResults = DataBaseManager::getInstance()->prepareAndExecuteQuery('SELECT * FROM users WHERE mail=? AND password=?', $args);
-        //Si la requête a fonctionné
-        if ($queryResults !== false) {
-            if (count($queryResults) == 1) {
-                $model = $queryResults[0];
-            }
-            else{
-                return false ;
-            }
-            return $model;
-        } else {
-            $model->dataError['connexion'] = "Impossible d'acceder a la table des utilisateurs";
-            return $model;
-        }
-    }
-
-
-   /** @brief Remplie des données de l'utilisateur à partir de la session
-	* @param $email email de l'utilisateur servant d'ID unique
-	* @param $role Rôle de l'utilisateur */
-   public static function getModelUserFromSession ($email , $role) {
-	   $model=new self(array());
-	   $model->role = $role;
-	   return $model;
-	}
-
-
+		/** @brief Connexion d'un user */
+	    public static function getModelConnexion($mail, $hashedPassword) {
+			$model = new self(array());
+			// Exécution de la requête via la classe de connexion (singleton). Le exceptions éventuelles, personnalisées, sont gérés par le Contrôleur
+	        $args = array($mail, $hashedPassword);
+	        $queryResults = DataBaseManager::getInstance()->prepareAndExecuteQuery('SELECT * FROM users WHERE mail=? AND password=?', $args);
+	        //Si la requête a fonctionné
+	        if ($queryResults !== false) {
+	            if (count($queryResults) == 1) {
+	                $model = $queryResults[0];
+	            }
+	            else{
+	                return false ;
+	            }
+	            return $model;
+	        } else {
+	            $model->dataError['connexion'] = "Impossible d'acceder a la table des utilisateurs";
+	            return $model;
+	        }
+	    }
 
     }
 ?>
