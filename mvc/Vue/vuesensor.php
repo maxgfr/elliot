cae4f5487bc318163ccd9b606706532c989b45b6
alphter <alphonse.terrier@isep.fr>
2017-12-11 11:39:08 +0100

    i

--- mvc/Vue/vuesensor.php
+++ mvc/Vue/vuesensor.php
@@ -40,6 +40,7 @@ th {
 </head>
 <body>
 
+
 <p>Click the button to add a new row at the first position of the table and then add cells and content.</p>
 
 <table id="sensor">
@@ -69,12 +70,11 @@ th {
 <button onclick="myFunction()">create</button>
 <button onclick="myDeleteFunction()">delete</button>
 <button onclick="ouvre_popup()">create formulaire</button>
-<button type="formulaire" onclick="toggle_text('formulaire');">formulaire</button>
-<span id="formulaire" style="display:none;">Bob</span>
-<button type="formulaire" onclick="toggle_text('formulaire');">
+<input type=button name=type id='formulaire' value='cree un capteur' onclick="setVisibility('nom');";> 
 
 
-  <div class="nom"  style="display:none;">
+  <div id="nom" style = 'display:none'>
+
     <fieldset>
       <legend>Nom</legend>
         <label for="Nom">Nom</label>
@@ -89,7 +89,6 @@ th {
           </select>
     </fieldset>
    </div> 
-</button>
 
 
 
@@ -115,41 +114,24 @@ function myDeleteFunction() {
 
 	}	
 
-  function ouvre_popup(page) {
-       window.open('vueformulairesensor.php',"nom_popup","menubar=no, status=no, scrollbars=no, menubar=no, width=200, height=100");
-   }
-
-$(function(){
-   $('formulaire').on('click'(function(){  
-      $('#nom').css("display","block");
-   });
-};
-
-
-
-function new_disp(){
-   document.getElementById('formulaire').style.display='block';
- }
+ 
 
 
 
 
-<button onclick='new_disp();'>
 
 
-    <div id="myform" style="display:none">
-
-
-
-function formulaire(id) {
-  var span = document.getElementById(id);
-  if(span.style.display == "none") {
-    span.style.display = "inline";
-  } else {
-    span.style.display = "none";
-  }
+function setVisibility(id) {
+if(document.getElementById('formulaire').value=='cree un capteur'){
+document.getElementById('formulaire').value = 'pas cree un capteur';
+document.getElementById(id).style.display = 'inline';
+}else{
+document.getElementById('formulaire').value = 'pas cree un capteur';
+document.getElementById(id).style.display = 'none';
+document.getElementById('formulaire').value = 'cree un capteur';
 }
 
+}
 
 </script>
 
