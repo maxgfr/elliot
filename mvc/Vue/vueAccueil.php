e248c8357ad2f635f0fe1ff5dade2ac92bf099ce
alphter <alphonse.terrier@isep.fr>
2017-12-12 09:43:42 +0100

    Bonjour

--- mvc/Vue/vueAccueil.php
+++ mvc/Vue/vueAccueil.php
@@ -1,13 +1,32 @@
 <?php
+
+  //Voir les erreurs
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL | E_STRICT);
+  session_start();
+  // Répertoire racine du MVC
+  $rootDirectory = dirname(__FILE__)."/../../mvc/";
+  // chargement de la classe Autoload pour autochargement des classes
+  require_once($rootDirectory.'Config/Autoload.php');
+  try {
+      Autoload::load();
+  } catch(Exception $e){
+      require (Config::getVues()["default"]) ;
+  }
+
+//Voir les erreurs
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
 // Répertoire racine du MVC
-$rootDirectory = dirname(__FILE__)."/../../mvc/";
+$rootDirectory = dirname(__FILE__) . "/../../mvc/";
 // chargement de la classe Autoload pour autochargement des classes
-require_once($rootDirectory.'Config\Autoload.php');
+require_once($rootDirectory . 'Config/Autoload.php');
 try {
     Autoload::load();
-} catch(Exception $e){
-    require (Config::getVues()["default"]) ;
+} catch (Exception $e) {
+    require(Config::getVues()["default"]);
 }
+
 ?>
 <!DOCTYPE html>
 <html>
