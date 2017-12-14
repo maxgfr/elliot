f9cc305dbcaaba1c335e7fe8bcf5b5e3d1e59d25
florianvavelin <vavelinflorian@gmail.com>
2017-12-14 21:58:16 +0100

    Page d'accueil : done

--- mvc/Vue/vueAccueil.php
+++ mvc/Vue/vueAccueil.php
@@ -32,8 +32,46 @@
                 <img src="../../img/bedroomIcon.png"/>
             </div>
             <div class="tablePart">
-                Température : 20°C
-                Humditié : 45%
+                <div class="tablePartCells tablePartCellsColorBlue" style="margin-left:5%">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/temperatureIcon.png" alt="Temperature Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        20°C
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorBlue">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/luminosityIcon.png" alt="Luminosity Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        40%
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorBlue" >
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/humidityIcon.png" alt="Humidity Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        35%
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorBlue">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/motionIcon.png" alt="Motion Icon">
+                    </div>
+                    <div class="tablePartCellsText" style="font-size:12px;margin-top:75%">
+                        Présence : NON
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorBlue">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/barometerIcon.png" alt="Motion Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        1013hPa
+                    </div>
+                </div>
             </div>
         </div>
         <div id="Kitchen_div">
@@ -41,6 +79,30 @@
                 <img src="../../img/kitchenIcon.png"/>
             </div>
             <div class="tablePart">
+                <div class="tablePartCells tablePartCellsColorRed" style="margin-left:5%">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/humidityIcon.png" alt="Humidity Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        29%
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorRed">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/luminosityIcon.png" alt="Luminosity Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        80%
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorRed" >
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/temperatureIcon.png" alt="Temperature Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        17°C
+                    </div>
+                </div>
             </div>
         </div>
         <div id="Bathroom_div">
@@ -48,6 +110,38 @@
                 <img src="../../img/showerIcon.png"/>
             </div>
             <div class="tablePart">
+                <div class="tablePartCells tablePartCellsColorGreen" style="margin-left:5%">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/humidityIcon.png" alt="Humidity Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        80%
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorGreen">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/luminosityIcon.png" alt="Luminosity Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        80%
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorGreen">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/temperatureIcon.png" alt="Temperature Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        35°C
+                    </div>
+                </div>
+                <div class="tablePartCells tablePartCellsColorGreen">
+                    <div class="tablePartCellsImage">
+                        <img src="../../img/barometerIcon.png" alt="Motion Icon">
+                    </div>
+                    <div class="tablePartCellsText">
+                        1000hPa
+                    </div>
+                </div>
             </div>
         </div>
     </div>
