3c006301cbc61231426b527d4432ba8535710fa1
Btvt <bryan.tovantrang@gmail.com>
2017-12-15 11:48:16 +0100

    Je n'ai pas touché à la page vueSupport.php. J'ai créé un doublon dans vueTicket.php et créé une vueFAQ.php

--- mvc/Config/Config.php
+++ mvc/Config/Config.php
@@ -17,12 +17,11 @@
 			$db_user ="root";
 			$db_password="root";
 			*/
-			/* CONFIG 2*/
-			$db_host="mysql:host=localhost;port=3306;";
+			/* CONFIG 2*			$db_host="mysql:host=localhost;port=3306;";
 			$db_name="dbname=elliot_db";
 			$db_user ="root";
 			$db_password="";
-
+			*/
 
 		}
 
---
+++ mvc/Vue/vueFAQ.php
@@ -0,0 +1,98 @@
+<?php
+  //Voir les erreurs
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL | E_STRICT);
+  // Répertoire racine du MVC
+  $rootDirectory = dirname(__FILE__)."/../../mvc/";
+  // chargement de la classe Autoload pour autochargement des classes
+  require_once($rootDirectory.'Config/Autoload.php');
+  try {
+      Autoload::load();
+  } catch(Exception $e){
+      require (Config::getVues()["default"]) ;
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <title> Support </title>
+    <link rel="stylesheet" href="../../css/style.css">
+    <link href="../../css/Support.css" rel="stylesheet" type="text/css"/>
+    <link rel="shortcut icon" href="../../img/smallellIoTICO.ico" />
+    <script src="../../js/animation.js"></script>
+
+
+</head>
+
+  <?php include("layouts/header.php"); ?>
+
+<body>
+
+    <div id="main">
+        
+        <h1> FAQ </h1>
+        <h4>Numéro du support : 08 99 12 34 56 (appel gratuit)</h4>
+        <h4>Mail du support : support.domisep@elliot.com</h4>
+        <br>  
+
+        <h3>I. Je souhaite connaître l'avancement de ma commande.</h3>  
+        <p>
+          Merci d'avoir choisi Domisep pour équiper votre domicile !
+          <br> N'hésitez pas à appeler directement le service Support Domisep 08 99 12 34 56 (appel gratuit) ou d'ouvrir un ticket de support via la page dédiée en mentionnant la section <em>Commande</em>.
+          <br> Nos conseillers vous informeront précisémennt  l'état de votre commande.
+        </p>
+        <br>
+
+        <h3>II. Mon capteur/HAGbox est cassé !</h3>  
+        <p>
+          Si vous détenez un appareil défaillant, nous vous prions de nous le renvoyer à l'adresse suivante :
+          <br> <strong>17 avenue de l'arnaque, 77700 Bailly-Romainvilliers.</strong>
+          <br> Veuillez joindre à votre colis une lettre décrivant votre situation ainsi que votre numéro client. Nous vous recontacterons dès réception de votre colis.
+        </p>
+        <br>
+
+
+        <h3>III. Je ne reçois plus d'information venant de "tel capteur" !</h3>
+        <p>
+          Si vous ne recevez plus aucune donnée venant d'un capteur, il se peut qu'il soit défaillant ou alors que la connexion du capteur au système général de Domisep soit brisée :
+        <ul>
+          <li> Dans le premier cas, veuillez vous référer à la question numéro II. </li>
+          <li> 
+            Dans le second cas, par souci de sécurité, il est conseillé d'effectuer un REBOOT :
+            <ul>
+              <li>Eteignez le capteur grâce au bouton associé. </li>
+              <li>Activez la recherche de capteurs par bluetooth via la page associée. </li>
+              <li>Rallumez le capteur grâce au bouton associé. </li>
+              <li>Attendez la confirmation de la connexion en vous plaçant de préférence près de la HAGbox. Cette reconnexion peut prendre quelques minutes.</li>
+            </ul>
+            Si votre problème persiste, nous vous conseillons d'appeler directement le service Support Domisep 08 99 12 34 56 (appel gratuit) ou d'ouvrir un ticket de support via la page dédiée en mentionnant la section <em>Connexion</em>.
+          </li>
+        </ul>
+          Dans le cas où votre capteur envoie certains paramètres (e.g. température par exemple), mais pas d'autres (e.g. détecteur de présence), veuillez vous référer à la question numéro II. 
+          <br>Dans le cas où vous ne recevez les données d'aucun capteur, la HAGbox peut être à l'origine du problème.
+          <br>Il faut alors REBOOT la HAGbox en la redémarrant et reconnecter les différents capteurs en effectuant la procédure précédente.
+          <br>Si le problème persiste, votre appareil peut être défaillant : veuillez vous référer à la question numéro II.
+        </p>  
+        <br>
+
+        <h3>IV. Je déménage et souhaite ré-aménager le système de capteurs à mon nouveau domicile.</h3>  
+        <p>
+          Le système général HAN a été conçu afin d'être facilement déployable : vous avez donc la possibilité de ré-aménager votre système à votre guise.
+          <br> Si vous souhaitez optimiser votre architecture en fonction du plan de votre nouveau domicile, nous serons ravi de vous envoyer un expert Domisep afin de déployer votre système dans les meilleures configurations et recevoir des suggestions supplémentaires pour votre confort. Dans le cas échéant, veuillez appeler directement le service Support Domisep 08 99 12 34 56 (appel gratuit) ou d'ouvrir un ticket de support via la page dédiée en mentionnant la section <em>Architecture</em>.
+        </p>
+        <br>
+
+        <h3>V. Je souhaite me désabonner de Domisep.</h3>  
+        <p>
+          Nous sommes désolés de l'apprendre ... 
+          <br>Si vous souhaitez rompre votre contrat, veuillez appeler directement le service Support Domisep 08 99 12 34 56 (appel gratuit) ou d'ouvrir un ticket de support via la page dédiée en mentionnant la section <em>Abonnement</em>.
+          <br>Afin de nous améliorer, nous souhaitons connaître la raison de votre départ en ouvrant un ticket de support via la page dédiée en mentionnant la section <em>Amélioration</em>.
+          <br>Nous vous remercions pour la confiance que vous nous avez apporté.
+        </p>
+        <br>
+
+    </div>
+</body>
+</html>
