197c8e0caff45521f0ce5327e97616351ce38ff6
Btvt <bryan.tovantrang@gmail.com>
2017-12-15 13:30:38 +0100

    Amélioration de vueNotifications.php

--- mvc/Vue/vueNotifications.php
+++ mvc/Vue/vueNotifications.php
@@ -17,6 +17,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/notification.css">
     <link rel="shortcut icon" href="../../img/smallellIoTICO.ico" />
     <script src="../../js/animation.js"></script>
     <title>Notifications</title>
@@ -25,7 +26,7 @@
 </head>
 <body>
     <div id="main">
-        <p> Vous avez <?php $notfi; ?> notifications</p>
+        <h1> Vous avez <?php $notfi; ?>5 notifications</h1>
         <table>
             <tr>
                 <th>Dates</th>
@@ -33,22 +34,54 @@
                 <th>Message</th>
             </tr>
 
+
+          <!--
             <tr>
                 <td><?php $insertdate; ?></td>
                 <td><?php $insertobjet; ?></td>
                 <td><?php $insertmessage; ?></td>
             </tr>
+          -->
+
+            <tr>
+                <td>29 Novembre 2017</td>
+                <td>Exemple 1</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+            </tr>
+
+            <tr>
+                <td>25 Novembre 2017</td>
+                <td>Exemple 2</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+            </tr>
+
+            <tr>
+                <td>19 Novembre 2017</td>
+                <td>Exemple 3</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+            </tr>
+
+            <tr>
+                <td>08 Octembre 2017</td>
+                <td>Exemple 4</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+            </tr>
+
+            <tr>
+                <td>17 Avril 1998</td>
+                <td>Exemple 5</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+            </tr>
+
         </table>
 
         <form>
             <div>
                 <p>Voulez-vous recevoir les notifications par sms ?</p>
-                <input type="checkbox" id="Oui" name="Oui" value="notification">
-                <label for="Oui">Oui</label>
-                <input type="checkbox" id="Non" name="Non" value="notification">
-                <label for="Non">Non</label>
+                Oui <input type="radio" id="YES" name="show_notification" value="yes"> <br>
+                Non<input type="radio" id="No" name="show_notification" value="no"> <br>
                 <br />
-                <input type="image" name="modification" value="modification" src="../img/Enregistrer_les_modifications.png">
+                <input type="submit" value="Enregistrer les modifications">
             </div>
         </form>
     </div>
