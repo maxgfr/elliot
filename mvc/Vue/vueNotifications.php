8fb56f95146a7f204a433b0a38258b2e1139275f
Maxime Golfier <maxgolfier@gmail.com>
2017-12-22 17:07:48 +0100

    ii

--- mvc/Vue/vueNotifications.php
+++ mvc/Vue/vueNotifications.php
@@ -1,19 +1,5 @@
 <?php
 
-//Voir les erreurs
-ini_set('display_errors', 'On');
-error_reporting(E_ALL | E_STRICT);
-// Répertoire racine du MVC
-$rootDirectory = dirname(__FILE__) . "/../../mvc/";
-// chargement de la classe Autoload pour autochargement des classes
-require_once($rootDirectory . 'Config/Autoload.php');
-try {
-    Autoload::load();
-} catch (Exception $e) {
-    require(Config::getVues()["default"]);
-}
-session_start();
-
   //Voir les erreurs
   ini_set('display_errors', 'On');
   error_reporting(E_ALL | E_STRICT);
