f642ecea8740fdd5c27d9c825e484652f582305e
rdcdt1 <wanduchalard@gmail.com>
2018-01-12 17:27:06 +0100

    j ai faim

---
+++ mvc/Modeles/MessageAjaxQuery.php
@@ -0,0 +1,20 @@
+<?php
+    // Répertoire racine du MVC
+    $rootDirectory = dirname(__FILE__)."/../../mvc/";
+    // chargement de la classe Autoload pour autochargement des classes
+    require_once($rootDirectory.'Config/Autoload.php');
+    try {
+      Autoload::load();
+    } catch(Exception $e){
+      require (Config::getVues()["default"]) ;
+    }
+
+    header("Content-Type: application/json; charset=UTF-8");
+    $obj = json_decode($_POST["x"], false);
+
+    $sql_query = "SELECT * FROM `message` WHERE `id_user` = ";
+
+    $query = DataBaseManager::getInstance()->prepareAndLaunchQuery($sql_query,array());
+
+    echo json_encode($query);
+?>
