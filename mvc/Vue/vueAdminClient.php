a15816489f3c8ddc21169261dc0e1d552687a9f4
Btvt <bryan.tovantrang@gmail.com>
2018-02-01 11:12:44 +0100

    vueAdminClient.php et vueAdmin_personne.php comments

--- mvc/Vue/vueAdminClient.php
+++ mvc/Vue/vueAdminClient.php
@@ -1,94 +1,121 @@
 <?php
-  //Voir les erreurs
-  ini_set('display_errors', 'On');
-  error_reporting(E_ALL | E_STRICT);
-  // Répertoire racine du MVC
-  $rootDirectory = dirname(__FILE__)."/../../mvc/";
-  // chargement de la classe Autoload pour autochargement des classes
-  require_once($rootDirectory.'Config/Autoload.php');
-  try {
-      Autoload::load();
-  } catch(Exception $e){
-      require (Config::getVues()["default"]) ;
-  }
-  session_start();
-  if(empty($_SESSION['email'])) {
-    header("Location:vueConnexion.php");
- }
+// Authorize errors to be displayed.
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
+// Navigate through MVC root directory
+$rootDirectory = dirname(__FILE__)."/../../mvc/";
+
+// Implement the "Autoload" class to load automatically all classes.
+require_once($rootDirectory.'Config/Autoload.php');
+try {
+	Autoload::load();
+} catch(Exception $e){
+	require (Config::getVues()["default"]) ;
+}
+
+session_start();
+if(empty($_SESSION['email'])) {
+	header("Location:vueConnexion.php");
+}
 ?>
+
+
+
+<!-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  -->
+<!--
+  This view displays informations about a particular client.
+  The user may acknowledge the client's subscription data 
+  and his current notifications for support purposes.
+-->
+<!-- //////////////////////////////////////////////////////////// -->
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
 
-    <meta charset="utf-8">
-    <title> DomISEP </title>
-    <link rel="stylesheet" href="../../css/profil.css">
-    <link href="../../css/notification.css" rel="stylesheet" type="text/css"/>
+	<meta charset="utf-8">
+	<title> DomISEP </title>
+	<link rel="stylesheet" href="../../css/profil.css">
+	<link href="../../css/notification.css" rel="stylesheet" type="text/css"/>
 
 </head>
 
-  <?php include("layouts/header.php"); ?>
+
+<?php include("layouts/header.php"); ?>
+
 
 <body>
 
-    <div id="main">
-        <div id='container_profile'>
-            <!-- Display/Set the user informations request. -->
-            <div id="container_contact_details">
-                <div id="contact_details_text">
-                    Fiche client
-                </div>
-                <div id="contact_details">
-                    <div class="profileData">
-                        <div class="textFromDatabase" style="margin-bottom:12px;">
-                            <div class="headerTable" id="headerTable"></div>
-                        </div>
-                        <div class="textFromDatabase" style="margin-bottom:12px;">
-                            <div id="addressTable"></div>
-                        </div>
-                        <div class="textFromDatabase" style="margin-bottom:12px;">
-                            <div id="mailTable"></div>
-                        </div>
-                        <div class="textFromDatabase" style="margin-bottom:12px;">
-                            <div id="birthdayTable"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="container_change_password">
-                <div id="change_password_text">
-                    Notifications
-                </div>
-                <div id="contact_details_input">
-                    <table>
-                        <tr>
-                            <td>La batterie du capteur 25643 doit être rechargée (10%).</td>
-                        </tr>
-                        <tr>
-                            <td>La connexion entre deux capteurs n'est plus assurée.</td>
-                        </tr>
-                        <tr>
-                            <td>Le capteur 23 n'est plus fonctionnel.</td>
-                        </tr>
-                        <tr>
-                            <td>Le capteur 6544 n'est plus fonctionnel.</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div id="main">
+		<div id='container_profile'>
+			<!-- Set the client subscription data template. -->
+			<div id="container_contact_details">
+				<div id="contact_details_text">
+					Fiche client
+				</div>
+				<div id="contact_details">
+					<div class="profileData">
+						<div class="textFromDatabase" style="margin-bottom:12px;">
+							<div class="headerTable" id="headerTable"></div>
+						</div>
+						<div class="textFromDatabase" style="margin-bottom:12px;">
+							<div id="addressTable"></div>
+						</div>
+						<div class="textFromDatabase" style="margin-bottom:12px;">
+							<div id="mailTable"></div>
+						</div>
+						<div class="textFromDatabase" style="margin-bottom:12px;">
+							<div id="birthdayTable"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Display/Set the client's current notifications. -->
+			<div id="container_change_password">
+				<div id="change_password_text">
+					Notifications
+				</div>
+				<div id="contact_details_input">
+					<table>
+						<tr>
+							<td>La batterie du capteur 25643 doit être rechargée (10%).</td>
+						</tr>
+						<tr>
+							<td>La connexion entre deux capteurs n'est plus assurée.</td>
+						</tr>
+						<tr>
+							<td>Le capteur 23 n'est plus fonctionnel.</td>
+						</tr>
+						<tr>
+							<td>Le capteur 6544 n'est plus fonctionnel.</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
 
+
+
+<!-- Display the client's subscription data. -->
 <script src="../../js/AdminClient.js"></script>
+
+
+
+<!-- Select the background color according to the user's status. -->
 <script>
-if (getCookie("cookie_toggle_state")==1) {
-    // if we are inside vueAdmin set the grey color
+
+	if (getCookie("cookie_toggle_state")==1) {
     $('#container_profile').css({"background-image":"linear-gradient(rgb(46,50,62), rgb(66,70,82))"})
 }
 if (getCookie("cookie_toggle_state")==0) {
-    // set the blue color
     $('#container_profile').css({"background-image":"linear-gradient(rgb(25, 50, 100), rgb(38, 67, 120))"})
 }
 
--- mvc/Vue/vueAdmin_personne.php
+++ mvc/Vue/vueAdmin_personne.php
@@ -1,75 +1,102 @@
 <?php
-//Voir les erreurs
+// Authorize errors to be displayed.
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
-// Répertoire racine du MVC
-$rootDirectory = dirname(__FILE__) . "/../../mvc/";
-// chargement de la classe Autoload pour autochargement des classes
-require_once($rootDirectory . 'Config/Autoload.php');
+
+// Navigate through MVC root directory
+$rootDirectory = dirname(__FILE__)."/../../mvc/";
+
+// Implement the "Autoload" class to load automatically all classes.
+require_once($rootDirectory.'Config/Autoload.php');
 try {
-    Autoload::load();
-} catch (Exception $e) {
-    require(Config::getVues()["default"]);
+	Autoload::load();
+} catch(Exception $e){
+	require (Config::getVues()["default"]) ;
 }
+
 session_start();
-if (empty($_SESSION['email'])) {
-    header("Location:vueConnexion.php");
+if(empty($_SESSION['email'])) {
+	header("Location:vueConnexion.php");
 }
 ?>
+
+
+
+<!-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  -->
+<!--
+  This view displays a list of accounts according to the 
+  user's input.
+  The user may insert keywords to find a specific
+  subscriber.
+-->
+<!-- //////////////////////////////////////////////////////////// -->
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title> Administration </title>
-    <link href="../../css/admin.css" rel="stylesheet" type="text/css"/>
-    <link href="../../css/admin_personne.css" rel="stylesheet" type="text/css"/>
-    <script src="../../js/jquery-3.2.1.min.js"></script>
+	<meta charset="utf-8">
+	<title> Administration </title>
+	<link href="../../css/admin.css" rel="stylesheet" type="text/css"/>
+	<link href="../../css/admin_personne.css" rel="stylesheet" type="text/css"/>
+	<script src="../../js/jquery-3.2.1.min.js"></script>
 
 </head>
+
+
 <?php include("layouts/header.php"); ?>
+
+
 <body>
-<div id="main">
 
-    <form method="post">
-        <fieldset>
-            <legend>Recherchez un utilisateur</legend>
-            <label for="type">Type :</label>
-            <select name="type" id="type">
-                <option value="last_name">Nom</option>
-                <option value="first_name">Prénom</option>
-                <option value="mail">Mail</option>
-                <option value="0">Client</option>
-                <option value="1">Admin</option>
-            </select>
-            <label for="input_search">Recherche :</label>
-            <input name="input_search" id="input_search">
-            <button type="button" id="search">Envoyer</button>
-        </fieldset>
-    </form>
-    <div id="result">
-        <table id="table_result">
-            <thead>
-            <tr>
-                <th>
-                    Nom
-                </th>
-                <th>
-                    Prénom
-                </th>
-                <th>
-                    Role
-                </th>
-                <th>
-                    Mail
-                </th>
-            </tr>
-            </thead>
-        </table>
+	<div id="main">
+		<!-- Set the form to input the keywords for the research. -->
+		<form method="post">
+			<fieldset>
+				<legend>Recherchez un utilisateur</legend>
+				<label for="type">Type :</label>
+				<select name="type" id="type">
+					<option value="last_name">Nom</option>
+					<option value="first_name">Prénom</option>
+					<option value="mail">Mail</option>
+					<option value="0">Client</option>
+					<option value="1">Admin</option>
+				</select>
+				<label for="input_search">Recherche :</label>
+				<input name="input_search" id="input_search">
+				<button type="button" id="search">Envoyer</button>
+			</fieldset>
+		</form>
 
-    </div>
-    <button type="submit" name="send_button" id="send_button">Submit</button>
-</div>
+		<!-- Set the list template to display the subscriber. -->
+		<div id="result">
+			<table id="table_result">
+				<thead>
+					<tr>
+						<th>
+							Nom
+						</th>
+						<th>
+							Prénom
+						</th>
+						<th>
+							Role
+						</th>
+						<th>
+							Mail
+						</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+		<button type="submit" name="send_button" id="send_button">Submit</button>
+	</div>
+
 </body>
 </html>
 
+
+
+<!-- Let the dynamic of the research to be displayed accordingly. -->
 <script src="../../js/admin_personne.js"></script>
